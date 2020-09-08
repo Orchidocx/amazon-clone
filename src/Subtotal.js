cfe8d47241eb7e3ba61b11b0a1537c6051366c01
Jacky Li <ilikemacaronii@gmail.com>
2020-09-08 14:34:08 -0700

    prepare subtotal assignment; add stateprovider

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import CurrencyFormat from 'react-currency-format';
 import './Subtotal.css';
 function Subtotal() {
   return (
     <div className='subtotal'>
-      
+      <CurrencyFormat
+      renderText={(value) => (
+        <>
+          <p>
+            {/* Part of the HW */}
+            Subtotal (0 items): <strong>0</strong>
+          </p>
+          <small className="subtotal__gift">
+            <input type="checkbox"/> This order contains a gift
+          </small>
+        </>
+      )}
+      decimalScale={2}
+      value={0} // assignment
+      displayType={"text"}
+      thousandSeparator={true}
+      prefix={"$"}
+      />
+      <button>Proceed to Checkout</button>
     </div>
   )
 }
