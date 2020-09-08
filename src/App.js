b0e7bdf3891aac765fb629ed54a20810c302bc41
Jacky Li <ilikemacaronii@gmail.com>
2020-09-08 12:18:06 -0700

    begin header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
+  // BEM Convention
   return (
     <div className="App">
-      <h1>AMAZON CLONE!</h1>
+      <Header />
+      {/* Header */}
+      {/* Home */}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBxw1u_zDLc3LhG39XBhoujImR8zeZ8Rrk",
+  authDomain: "clone-d5e88.firebaseapp.com",
+  databaseURL: "https://clone-d5e88.firebaseio.com",
+  projectId: "clone-d5e88",
+  storageBucket: "clone-d5e88.appspot.com",
+  messagingSenderId: "496619459649",
+  appId: "1:496619459649:web:a6ad87fe945f7bc08662c0",
+  measurementId: "G-Z6EHK3Z2L9"
+};
