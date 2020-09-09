32dec2ed440e73214c41bbe251912142c91f423f
Jacky Li <ilikemacaronii@gmail.com>
2020-09-08 21:01:13 -0700

    adjust products

--- src/Home.js
+++ src/Home.js
@@ -10,51 +10,53 @@ function Home() {
 
         <div className="home__row">
           <Product 
-            id='100'
-            title='The lean startup' 
-            price={29.99} 
-            image={'https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'}
+            id='100' key={100}
+            title='
+            Apple iPad (10.2inch, Wi-Fi, 128GB) - Gold (Latest Model)' 
+            price={399.99} 
+            image={'https://images-na.ssl-images-amazon.com/images/I/61NhHYq8AnL._AC_SL1500_.jpg'}
             rating={5}
           />
           <Product 
-            id='101'
-            title='The lean startup' 
-            price={29.99} 
-            image={'https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'}
-            rating={5}
-          />
-        </div>
-        
-        <div className="home__row">
-          <Product 
-            id='102'
-            title='Fitbit Companion Bracelet' 
-            price={199.99} 
-            image={'https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg'}
-            rating={5}
-          />
-          <Product 
-            id='103'
-            title='The lean startup' 
-            price={29.99} 
-            image={'https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'}
-            rating={5}
-          />
-          <Product 
-            id='104'
-            title='The lean startup' 
-            price={29.99} 
-            image={'https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'}
+            id='101' key={101}
+            title='
+            AMD Ryzen 5 3600X 6-Core, 12-Thread Unlocked Desktop Processor with Wraith Spire Cooler' 
+            price={202.09} 
+            image={'https://images-na.ssl-images-amazon.com/images/I/616i5Il16qL._AC_SL1000_.jpg'}
             rating={5}
           />
         </div>
         
         <div className="home__row">
           <Product 
-            id='105'
-            title='The lean startup' 
-            price={29.99} 
-            image={'https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'}
+            id='102' key={102}
+            title='Apple AirPods with Charging Case (Wired)' 
+            price={159.00} 
+            image={'https://images-na.ssl-images-amazon.com/images/I/71NTi82uBEL._AC_SL1500_.jpg'}
+            rating={5}
+          />
+          <Product 
+            id='103' key={103}
+            title='NZXT H510 - CA-H510B-W1 - Compact ATX Mid-Tower PC Gaming Case' 
+            price={69.94} 
+            image={'https://images-na.ssl-images-amazon.com/images/I/71QSKpbzlQL._AC_SL1500_.jpg'}
+            rating={5}
+          />
+          <Product 
+            id='104' key={104}
+            title='Gigabyte GV-N1030OC-2GI Nvidia GeForce GT 1030 OC 2G Graphics Card' 
+            price={88.99} 
+            image={'https://images-na.ssl-images-amazon.com/images/I/919lkVdI%2BbL._AC_SL1500_.jpg'}
+            rating={5}
+          />
+        </div>
+        
+        <div className="home__row">
+          <Product 
+            id='105' key={105}
+            title='Samsung CJ890 Series 49 inch 3840x1080 Super Ultra-Wide Desktop Monitor for Business, 144 Hz, USB-C, HDMI, DisplayPort, 3-Year Warranty (C49J890DKN)' 
+            price={1200.99} 
+            image={'https://images-na.ssl-images-amazon.com/images/I/61Hh-0F7AbL._AC_SL1000_.jpg'}
             rating={5}
           />
         </div>
