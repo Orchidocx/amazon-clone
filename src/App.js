56fcbb7e81924a3f20f7bad3dfc564591f6e177a
Jacky Li <ilikemacaronii@gmail.com>
2020-09-09 15:33:18 -0700

    finished user authentication

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './Login';
+import { auth } from './firebase';
+import { useStateValue } from './StateProvider';
 
 function App() {
-  
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // runs once when the app component loads
+    auth.onAuthStateChanged(authUser => {
+      console.log('THE USER IS >>>', authUser);
+      if(authUser) {
+        // User just logged in / user was logged in
+        dispatch({type: 'SET_USER', user: authUser});
+      } else {
+        // user is logged off
+        dispatch({type: 'SET_USER', user: null});
+      }
+    });
+  }, [/* if empty, only run once when app component loads;*/]);
   return (
     // BEM Convention
     <Router>
