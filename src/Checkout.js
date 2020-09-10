743349099a28a1c4fd5932baf8fcd543855c694e
Jacky Li <ilikemacaronii@gmail.com>
2020-09-10 15:43:35 -0700

    update user display

--- src/Checkout.js
+++ src/Checkout.js
@@ -4,12 +4,13 @@ import Subtotal from './Subtotal';
 import CheckoutProduct from './CheckoutProduct';
 import { useStateValue } from './StateProvider';
 function Checkout() {
-  const [{basket}, dispatch] = useStateValue();
+  const [{basket, user}, dispatch] = useStateValue();
   return (
     <div className='checkout'>
       <div className="checkout__left">
         <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" className="checkout__ad"/>
         <div>
+          <h3>Hello {user?.email || 'Guest'}</h3>
           <h2 className="checkout__title">Your Shopping Basket</h2>
           {basket.map(item=> (
             <CheckoutProduct
