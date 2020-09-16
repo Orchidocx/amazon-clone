1b8de42934e52900e86a53e952654ca5c2e7c2d8
Jacky Li <ilikemacaronii@gmail.com>
2020-09-15 19:24:39 -0700

    add payment css; update axios api url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5001/clone-d5e88/us-central1/api'
+  baseURL: 'https://us-central1-clone-d5e88.cloudfunctions.net/api'
+  // 'http://localhost:5001/clone-d5e88/us-central1/api'
 });
 
 export default instance;
+
+// Production
+// https://us-central1-clone-d5e88.cloudfunctions.net/api
+// Debugging
+// http://localhost:5001/clone-d5e88/us-central1/api
